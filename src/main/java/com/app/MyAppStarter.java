d5839709eca8faba90a9ae46598fb488c63a2bba
My Pc <My Pc@192.168.0.3>
2019-06-06 09:44:47 +0530

    First time

---
+++ src/main/java/com/app/MyAppStarter.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppStarter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(MyAppStarter.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/runner/ConsoleRunner.java
@@ -0,0 +1,40 @@
+package com.app.runner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsoleRunner implements ApplicationRunner{
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// ---Option args---
+		//1.read all keys
+		Set<String> optionKeys = args.getOptionNames();
+		System.out.println(optionKeys);
+		
+		//2.one key related values
+		List<String> optionValues = args.getOptionValues("profile");
+		System.out.println(optionValues);
+		
+		//3.check for key exist
+		boolean containsOption = args.containsOption("nature");
+		boolean containsOption2 = args.containsOption("db");
+		System.out.println(containsOption);
+		System.out.println(containsOption2);
+		
+		//---Non Option Args---
+		List<String> nonOptionArgs = args.getNonOptionArgs();
+		System.out.println(nonOptionArgs);
+		
+		//---all args---
+		String[] sourceArgs = args.getSourceArgs();
+		System.out.println(sourceArgs);
+		System.out.println(Arrays.asList(sourceArgs));
+		
+	}
+}
